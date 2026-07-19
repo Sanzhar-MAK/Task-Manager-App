8e2e915bbd2de5b73cd901d88bd080aa4a5b5544
Sanzhar <sanzarmakasev@gmail.com>
2026-07-19 23:20:38 +0500

    Start to create UserLoginForm

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,13 +1,16 @@
 from django import forms
 from .models import Task
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ('title', 'description','completed')
 
+
+class UserLoginForm(AuthenticationForm):
+     pass
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(
