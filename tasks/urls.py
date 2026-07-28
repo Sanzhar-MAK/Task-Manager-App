e8282243970283e9a043309c48046b0bb8f9d9d6
Sanzhar <sanzarmakasev@gmail.com>
2026-07-28 21:59:30 +0500

    Finish logic for user logout

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.user_login, name='user_login'),
     path('register', views.user_register, name='user_register'),
+    path('logout', views.user_logout, name='user_logout'),
     path('tasks', views.task_list, name='task_list'),
     path('task/<int:pk>/', views.task_detail, name='task_detail'),
     path('task/new/', views.task_new, name='task_new'),
