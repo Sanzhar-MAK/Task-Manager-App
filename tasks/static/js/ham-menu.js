a7b992c9137cb317c97f8b69b676a4da1d704161
Sanzhar <sanzarmakasev@gmail.com>
2026-08-07 22:52:48 +0500

    Change style css

--- tasks/static/js/ham-menu.js
+++ tasks/static/js/ham-menu.js
@@ -1,7 +1,12 @@
 const hamMenu = document.querySelector('.header-ham-menu')
 const offScreenMenu = document.querySelector('.header-off-screen-menu')
+// const addNewTask = document.querySelector('.add-new-task')
 
 hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
     offScreenMenu.classList.toggle('active');
 })
+
+// addNewTask.addEventListener('click', () => {
+//     this.remove();
+// })
