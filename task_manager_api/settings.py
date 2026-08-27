56ceb2c775572ded7768139a1fbd01bd6937a322
Sanzhar <sanzarmakasev@gmail.com>
2026-08-27 23:05:52 +0500

    Add token to setting

--- task_manager_api/settings.py
+++ task_manager_api/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tasks',
-    'rest_framework'
+    'rest_framework',
+    'rest_framework.authtoken'
 ]
 
 REST_FRAMEWORK = {
