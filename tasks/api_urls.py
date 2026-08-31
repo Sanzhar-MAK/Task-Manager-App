6cd5e965610a2651f3e6659ee3675816c6b13f7e
Sanzhar <sanzarmakasev@gmail.com>
2026-08-31 23:00:50 +0500

    Add Generic View in the views

--- tasks/api_urls.py
+++ tasks/api_urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from . api_views import TaskListCreateApiView
 from . import api_views
 
 urlpatterns = [
-    path('api/tasks/', api_views.task_api_list, name='task_api_list'),
-    path('api/tasks/<int:pk>', api_views.task_api_detail, name='task_api_detail')
+    # path('api/tasks/', api_views.task_api_list, name='task_api_list'),
+    path('api/tasks/', TaskListCreateApiView.as_view(), name='task_api_list'),
+    path('api/tasks/<int:pk>', api_views.task_api_detail, name='task_api_detail'),
 ]
--- tasks/api_views.py
+++ tasks/api_views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, generics
 from rest_framework.permissions import IsAuthenticated
 
 from .models import Task
 from .serializers import TaskSerializer
+
+class TaskListCreateApiView(generics.ListCreateAPIView):
+    serializer_class = TaskSerializer
+    def get_queryset(self):
+        return Task.objects.filter(author=self.request.user)
 
 
 @api_view(["GET", "POST"])
