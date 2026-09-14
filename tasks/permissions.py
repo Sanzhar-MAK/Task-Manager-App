e3c95515f47b4fc91e4362c5da8105ab0e0a286f
Sanzhar <sanzarmakasev@gmail.com>
2026-09-14 22:34:19 +0500

    Continue working with permissions

--- tasks/permissions.py
+++ tasks/permissions.py
@@ -1,5 +1,8 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+from rest_framework import permissions
 
 class IsOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
-        return request.author == obj.author
+        if request.method in SAFE_METHODS:
+            return True
+        return request.user == obj.author
